21d40734cad1eb83b2be581c5b0447d84869b378
vvivalagloria <alisa.legkostup.10@gmail.com>
2025-06-05 08:42:55 +0300

    homework

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,195 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:course_project/services/database_server.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sign_button/sign_button.dart';
+import '../modules/todo.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _textEditingController = TextEditingController();
+  final DatabaseService _databaseService = DatabaseService();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth.authStateChanges().listen((event) {
+      setState(() {
+        _user = event;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: _appBar(),
+      body:
+          _user != null
+              ? Column(children: [_userInfo()])
+              : Center(child: _googleSignInButton()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _displayTextInputDialog,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _appBar() {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: const Text("Todo", style: TextStyle(color: Colors.white)),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(child: Column(children: [_messagesListView()]));
+  }
+
+  Widget _googleSignInButton() {
+    return Center(
+      child: SizedBox(
+        height: 50,
+        child: SignInButton(
+          buttonType: ButtonType.google,
+          onPressed: () {
+            _handleUserSignIn();
+          },
+        ),
+      ),
+    );
+  }
+
+  void _handleUserSignIn() {
+    try {
+      GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
+      _auth.signInWithProvider(_googleAuthProvider);
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Widget _userInfo() {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * 0.80,
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage(_user!.photoURL!)),
+            ),
+          ),
+          Text(_user!.email!),
+          Text(_user!.displayName ?? ""),
+          MaterialButton(
+            color: Colors.red,
+            child: const Text("Sign Out"),
+            onPressed: _auth.signOut,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _messagesListView() {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * 0.80,
+      width: MediaQuery.sizeOf(context).width,
+      child: StreamBuilder(
+        stream: _databaseService.getTodos(),
+        builder: (context, snapshot) {
+          List todos = snapshot.data?.docs ?? [];
+          if (todos.isEmpty) {
+            return const Center(child: Text("Add a todo!"));
+          }
+          return ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              Todo todo = todos[index].data();
+              String todoId = todos[index].id;
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                child: ListTile(
+                  tileColor: Theme.of(context).colorScheme.primaryContainer,
+                  title: Text(todo.task),
+                  subtitle: Text(
+                    DateFormat(
+                      "dd-MM-yyyy h:mm a",
+                    ).format(todo.updatedOn.toDate()),
+                  ),
+                  trailing: Checkbox(
+                    value: todo.isDone,
+                    onChanged: (value) {
+                      Todo updatedTodo = todo.copyWith(
+                        isDone: !todo.isDone,
+                        updatedOn: Timestamp.now(),
+                      );
+                      _databaseService.updateTodo(todoId, updatedTodo);
+                    },
+                  ),
+                  onLongPress: () {
+                    _databaseService.deleteTodo(todoId);
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void _displayTextInputDialog() async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add a todo'),
+          content: TextField(
+            controller: _textEditingController,
+            decoration: const InputDecoration(hintText: "Todo..."),
+          ),
+          actions: <Widget>[
+            MaterialButton(
+              color: Theme.of(context).colorScheme.primary,
+              textColor: Colors.white,
+              child: const Text('Ok'),
+              onPressed: () {
+                Todo todo = Todo(
+                  task: _textEditingController.text,
+                  isDone: false,
+                  createdOn: Timestamp.now(),
+                  updatedOn: Timestamp.now(),
+                );
+                _databaseService.addTodo(todo);
+                Navigator.pop(context);
+                _textEditingController.clear();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/services/database_server.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../modules/todo.dart';
+
+const String TODO_COLLECTION_REF = "todos";
+
+class DatabaseService {
+  final _firestore = FirebaseFirestore.instance;
+
+  late final CollectionReference _todosRef;
+
+  DatabaseService() {
+    _todosRef = _firestore
+        .collection(TODO_COLLECTION_REF)
+        .withConverter<Todo>(
+          fromFirestore: (snapshots, _) => Todo.fromJson(snapshots.data()!),
+          toFirestore: (todo, _) => todo.toJson(),
+        );
+  }
+
+  Stream<QuerySnapshot> getTodos() {
+    return _todosRef.snapshots();
+  }
+
+  void addTodo(Todo todo) async {
+    _todosRef.add(todo);
+  }
+
+  void updateTodo(String todoId, Todo todo) {
+    _todosRef.doc(todoId).update(todo.toJson());
+  }
+
+  void deleteTodo(String todoId){
+    _todosRef.doc(todoId).delete();
+  }
+}
+
+
